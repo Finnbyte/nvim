8e531278a69460a972a992ca7a7a9bd263087b96
bytz <bytz@yimi.me>
2023-02-03 17:25:26 +0200

    Remove a lot of obsolete/old keybindings

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,4 +1,4 @@
--- Helper functions
+-- Helper functions (available globally by design)
 function map(mode, shortcut, command)
   vim.keymap.set(mode, shortcut, command, { noremap = true, silent = true })
 end
@@ -10,15 +10,10 @@ end
 -- Set <leader> key
 vim.g.mapleader = ' ' -- Map leader key to space
 
--- Actual mappings
 map('n', '<C-n>', ':set rnu!<CR>') -- toggle relative line numbers
 
 map("n", "0", "^")
 map("n", "^", "0")
-
--- Browse dotfiles (cross-platform)
--- map("n", "<leader>cf", ":exe ':Ex ' . stdpath('config')<CR>")
--- map("n", "<leader>cf", vim.cmd.NeoTreeShowToggle(vim.fn.stdpath('config')))
 
 -- Switch windows
 map("n", "<C-c>", "<C-w>c")
@@ -27,15 +22,5 @@ map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
--- Open file manager in Vim
--- map("n", "<leader>.", ":Ex<CR>")
-
 -- Disable highlighted entries
 map("n", "<F2>", ":nohlsearch<CR>")
-
-map("n", "<leader>r", ":w <Bar> :so %<CR>")
-map("n", "<leader>w", ":w<CR>")
-map("n", "<Leader><Leader>", ":b#<CR>")
-map("n", "<Leader>b", ":BufExplorer<CR>")
-map("n", "<leader>m", ":NERDTreeToggle<CR>")
-map("n", "<leader>rt", ":!ctags -R .<CR><CR>")
