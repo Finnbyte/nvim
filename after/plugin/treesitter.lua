8d51a71d7fd28207013c8c4722e0ac18d449affa
bytz <bytz@yimi.me>
2023-02-03 16:48:03 +0200

    Add html and css to ensure_installed parsers for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "json", "http", "help", "typescript", "javascript", "python", "go", "c", "lua", "rust" },
+  ensure_installed = { "json", "http", "html", "css", "help", "typescript", "javascript", "python", "go", "c", "lua", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
