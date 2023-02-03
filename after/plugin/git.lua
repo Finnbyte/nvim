82b81b10fedfbd211e2c6caed63113dc6c2fb95c
bytz <bytz@yimi.me>
2023-02-03 17:23:53 +0200

    Make universal git.lua file where all git-related plugins are configured

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,1 +1,14 @@
-map("n", "<leader>gs", vim.cmd.Git)
+map("n", "<leader>gs", vim.cmd.Git) -- Fugitive
+
+require('neogit').setup {} -- neogit
+
+-- Lazygit 
+vim.g.lazygit_floating_window_winblend = 1 -- transparency of floating window
+vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+vim.g.lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'} -- customize lazygit popup window corner characters
+vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+vim.g.lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
+vim.g.lazygit_config_file_path = '' -- custom config file path
+
+map("n", "<leader>gg", vim.cmd.LazyGit)
