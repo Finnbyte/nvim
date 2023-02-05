45ca17f9835cb4d081733b11131a3f30b6c4cc72
bytz <bytz@yimi.me>
2023-02-05 20:26:56 +0200

    Add buffer deleting key in telescope "buffers" tool

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -12,12 +12,33 @@ require("telescope").setup {
     }
 }
 
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
 -- Handle builtins
 local builtin = require('telescope.builtin')
 map("n", "<leader>f.", builtin.find_files)
 map("n", "<leader>f,", builtin.git_files)
-map("n", "<leader>fr", builtin.oldfiles, {})
-map("n", "<leader>fg", function() builtin.live_grep() end, {})
-map("n", "<leader>/", function() builtin.grep_string({ search = vim.fn.input("Grep > "), regex = true }) end, {})
-map("n", "<leader>bb", builtin.buffers, {})
+map("n", "<leader>fr", builtin.oldfiles)
+map("n", "<leader>fg", function() builtin.live_grep() end)
+map("n", "<leader>/", function() builtin.grep_string({ search = vim.fn.input("Grep > "), regex = true }) end)
+
+-- Buffers
+local del_buf = function(prompt_bufnr)
+    local selection = action_state.get_selected_entry()
+    actions.close(prompt_bufnr)
+    vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+end
+
+map("n", "<leader>bb", function() builtin.buffers{
+    attach_mappings = function(prompt_bufnr , map)
+        map('n', 'dd', function()
+            del_buf(prompt_bufnr)
+            builtin.buffers()
+        end)
+        return true
+    end
+} end)
+
+-- Help menu
 map("n", "<leader>h", builtin.help_tags, {})
