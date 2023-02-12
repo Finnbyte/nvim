d954ecdf4733680d3c632ac0c07da60d12a26a6d
bytz <bytz@yimi.me>
2023-02-12 21:52:46 +0200

    Install impatient and refractor indentation

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,14 +1,14 @@
 -- Find out if Packer already installed.
 -- Clone from Github if not.
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -17,6 +17,8 @@ require("packer").startup({
     function(use)
         -- Packer can manage it self
         use { "wbthomason/packer.nvim" }
+
+        use { 'lewis6991/impatient.nvim' }
 
         -- Plugins I want only in a Linux environment
         if vim.fn.has("linux") or vim.fn.has("wsl") then
@@ -52,41 +54,39 @@ require("packer").startup({
             "williamboman/mason-lspconfig.nvim", -- Bridges gap between mason and LSP
             "williamboman/mason.nvim", -- Downloading language servers
             "neovim/nvim-lspconfig" } -- LSP
-        use {
-            "nvim-telescope/telescope.nvim", tag = "0.1.0",
-            -- or                            , branch = "0.1.x",
-            requires = { {"nvim-lua/plenary.nvim"} }
-        }
-        use { "lukas-reineke/indent-blankline.nvim" }
-        use { "numToStr/Comment.nvim" } -- Extreme power over comments 
+            use {
+                "nvim-telescope/telescope.nvim", tag = "0.1.0",
+                -- or                            , branch = "0.1.x",
+                requires = { {"nvim-lua/plenary.nvim"} }
+            }
+            use { "lukas-reineke/indent-blankline.nvim" }
+            use { "numToStr/Comment.nvim" } -- Extreme power over comments 
 
-        -- Git
-        use { "tpope/vim-fugitive"}
-        use { "kdheepak/lazygit.nvim" }
-        use { "lewis6991/gitsigns.nvim" }
+            -- Git
+            use { "tpope/vim-fugitive"}
+            use { "kdheepak/lazygit.nvim" }
+            use { "lewis6991/gitsigns.nvim" }
 
-        -- File browser(s)
-        use { "tpope/vim-vinegar"}
+            -- File browser(s)
+            use { "tpope/vim-vinegar"}
 
-        -- Which key
-        use { "folke/which-key.nvim" }
+            -- Which key
+            use { "folke/which-key.nvim" }
 
-        -- Writing
-        use { "junegunn/goyo.vim" }
+            -- Writing
+            use { "junegunn/goyo.vim" }
 
-        -- Misc Tim Pope
-        use { "kylechui/nvim-surround" } -- Change surrounding quotes / parens easily
+            -- Discord
+            use { "andweeb/presence.nvim" } -- Discord stuff :P
 
-        -- Discord
-        use { "andweeb/presence.nvim" } -- Discord stuff :P
+            -- Notetaking
+            use { "vimwiki/vimwiki" } -- Personal wiki in Vim
 
-        -- Notetaking
-        use { "vimwiki/vimwiki" } -- Personal wiki in Vim
 
-        if packer_bootstrap then
-            require("packer").sync()
-        end
-    end,
+            if packer_bootstrap then
+                require("packer").sync()
+            end
+        end,
         -- display packer dialouge in the center in a floating window
         config = {
             display = {
