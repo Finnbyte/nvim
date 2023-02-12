770787e0c5f152a5d271ed7ae4f39d670e68454f
bytz <bytz@yimi.me>
2023-02-12 22:52:17 +0200

    Change MiniJump2d to work properly in normal, visual

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -11,7 +11,7 @@ require("mini.statusline").setup({
 })
 
 require("mini.jump2d").setup()
-map("n", "<CR>", '<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<CR>', {})
+map({"n", "v"}, "<CR>", '<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<CR>', {})
 
 require("mini.pairs").setup({
     -- Modes where enabled
