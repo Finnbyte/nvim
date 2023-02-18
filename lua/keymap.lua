c98942a4839ed941b0720f66fa18c1694bef55b1
bytz <bytz@yimi.me>
2023-02-18 20:59:31 +0200

    Add buffer switching functionality to terminal mode

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -18,9 +18,9 @@ map("n", "0", "^")
 map("n", "^", "0")
 
 -- Browse buffers
-map({"n", "i"}, "<C-h>", vim.cmd.bprev)
-map({"n", "i"}, "<M-d>", vim.cmd.bdelete)
-map({"n", "i"}, "<C-l>", vim.cmd.bnext)
+map({"n", "i", "t"}, "<C-h>", vim.cmd.bprev)
+map({"n", "i", "t"}, "<M-d>", vim.cmd.bdelete)
+map({"n", "i", "t"}, "<C-l>", vim.cmd.bnext)
 
 -- Switch windows
 -- map("n", "<C-c>", "<C-w>c")
