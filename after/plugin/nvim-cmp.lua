edb3d60ce40ff9dcefa46cb9421763ac87126b02
Finnbyte <eelpy@protonmail.com>
2023-02-20 19:48:56 +0200

    Transfer mapping to nvim-cmp.lua

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -1,9 +1,30 @@
 vim.opt.completeopt = { "menu", "menuone", "noselect" }-- Make cmp better? Or required? Dunno.
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
 -- Setup nvim-cmp.
-local cmp = require 'cmp'
+local cmp = require("cmp")
 
 cmp.setup({
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+                return vim_item
+            end
+        })
+    },
     snippet = {
         expand = function(args)
             -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
@@ -17,6 +38,28 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+                -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
         ['<C-k>'] = cmp.mapping.scroll_docs(-4),
         ['<C-j>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -1,19 +1,1 @@
--- Mappings
--- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
--- inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-local luasnip = require("luasnip")
-
-map("i", "<Tab>", function()
-    if luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-    else
-        return "<Tab>"
-    end
-end, { silent = true, expr = true, replace_keycodes = true })
-
-map("i", "<S-Tab>", function()
-    luasnip.jump(-1)
-end, { silent = true })
-
 require("luasnip.loaders.from_vscode").lazy_load() -- VSCode-like snippets
