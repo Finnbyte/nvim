70650e0960fe497e9963b7e393f511afa8c23553
bytz <bytz@yimi.me>
2023-02-28 08:50:31 +0200

    Configure mini-comment and disable old comment plugin

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -39,3 +39,31 @@ require("mini.pairs").setup({
 
 
 require("mini.surround").setup()
+
+
+require('mini.comment').setup({
+    options = {
+        -- Whether to ignore blank lines when adding comment
+        ignore_blank_line = false,
+
+        -- Whether to recognize as comment only lines without indent
+        start_of_line = false,
+    },
+    mappings = {
+        -- Toggle comment (like `gcip` - comment inner paragraph) for both
+        -- Normal and Visual modes
+        comment = 'gc',
+
+        -- Toggle comment on current line
+        comment_line = 'gcc',
+
+        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+        textobject = 'gc',
+    },
+    hooks = {
+        -- Before successful commenting. Does nothing by default.
+        pre = function() end,
+        -- After successful commenting. Does nothing by default.
+        post = function() end,
+    },
+})
