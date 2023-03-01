ab8e4f2a72a38fee07863ddb63ca143e6b191f0b
bytz <bytz@yimi.me>
2023-03-01 08:48:46 +0200

    Add new mini modules

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -14,6 +14,12 @@ require("mini.statusline").setup({
 })
 
 
+require("mini.move").setup()
+
+
+require("mini.jump").setup()
+
+
 require("mini.jump2d").setup()
 map({"n", "v"}, "<CR>", '<Cmd>lua MiniJump2d.start(MiniJump2d.builtin_opts.single_character)<CR>')
 
