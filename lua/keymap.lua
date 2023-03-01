2ad87b2e3900f8bfde25635445cb6ea3bf7947e6
bytz <bytz@yimi.me>
2023-03-01 08:49:47 +0200

    Add new keybindings for smoother screen upping/downing

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,8 +7,8 @@ function unmap(mode, bind)
 	vim.api.nvim_del_keymap(mode, bind)
 end
 
--- Set <leader> key
 vim.g.mapleader = ' ' -- Map leader key to space
+-- Set <leader> key
 
 map('n', '<F1>', ':set rnu!<CR>') -- toggle relative line numbers
 map("n", "<F2>", ":nohlsearch<CR>") -- Disable highlighted entries
@@ -19,8 +19,18 @@ map("n", "^", "0")
 
 -- Browse buffers
 map({"n", "i", "t"}, "<C-h>", vim.cmd.bprev)
-map({"n", "i", "t"}, "<M-d>", vim.cmd.bdelete)
 map({"n", "i", "t"}, "<C-l>", vim.cmd.bnext)
+map({"n", "i", "t"}, "<M-d>", vim.cmd.bdelete)
+
+-- Resize splits
+-- map({"n", "i", "t"}, "<C-Right>", vim.cmd([[resize +10]]))
+-- map({"n", "i", "t"}, "<C-Left", vim.cmd([[resize -10]]))
+-- map({"n", "i", "t"}, "<C-Up", vim.cmd([[vertical resize +10]]))
+-- map({"n", "i", "t"}, "<C-Down>", vim.cmd([[vertical resize -10]]))
+
+-- Go up/down 1/2 screen without moving cursor
+map({"n", "i", "t", "v"}, "<C-u>", "<C-u>zz")
+map({"n", "i", "t", "v"}, "<C-d>", "<C-d>zz")
 
 -- Switch windows
 -- map("n", "<C-c>", "<C-w>c")
