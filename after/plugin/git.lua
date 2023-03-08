850490ac76b9d254c13a846f6f95151fe5db5206
bytz <bytz@yimi.me>
2023-03-08 08:40:53 +0200

    Make lazygit plugin use lazygit config

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -7,7 +7,7 @@ vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floatin
 vim.g.lazygit_floating_window_corner_chars = {'╭', '╮', '╰', '╯'} -- customize lazygit popup window corner characters
 vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
 vim.g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
-vim.g.lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
-vim.g.lazygit_config_file_path = '' -- custom config file path
+vim.g.lazygit_use_custom_config_file_path = 1 -- config file path is evaluated if this value is 1
+vim.g.lazygit_config_file_path = vim.fn.stdpath("config") .. "/lazygit/config.yaml" -- custom config file path
 
 Map("n", "<leader>gg", vim.cmd.LazyGit)
