914480707469ad52cfdb0959f38d56a5b9d9b07d
Finnbyte <eelpy@protonmail.com>
2023-03-08 16:13:45 +0200

    add snippets for react

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -1,1 +1,3 @@
 require("luasnip.loaders.from_vscode").lazy_load() -- VSCode-like snippets
+require('luasnip').filetype_extend("javascript", { "javascriptreact" })
+require('luasnip').filetype_extend("javascript", { "html" })
