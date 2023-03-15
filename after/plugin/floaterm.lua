e5bc2fdd3b5162549ae49fa0db07a8f88fcd99b0
bytz <bytz@yimi.me>
2023-03-15 08:25:21 +0200

    Fix lazygit listening for floaterm mappings

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,14 +1,16 @@
 vim.g.floaterm_autoinsert = true
 
-Map({"n", "t"}, "<leader>t", vim.cmd.FloatermToggle)
-Map({"n", "t"}, "<leader>tn", vim.cmd.FloatermNew)
+Map("n", "<leader>t", vim.cmd.FloatermToggle)
+Map("n", "<leader>tn", vim.cmd.FloatermNew)
 
-vim.api.nvim_create_autocmd({"FileType"}, {
+vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = "floaterm",
     callback = function()
         BufMap("t", "<Esc>", [[<C-\><C-n>]])
         BufMap("t", "<C-h>", vim.cmd.FloatermPrev)
         BufMap("t", "<C-l>", vim.cmd.FloatermNext)
         BufMap("t", "<C-k>", vim.cmd.FloatermKill)
+        BufMap("t", "<leader>t", vim.cmd.FloatermToggle)
+        BufMap("t", "<leader>tn", vim.cmd.FloatermNew)
     end
 })
