78946612b60ecf2dd9b0e9f8c26cdba4ba796088
Finnbyte <eelpy@protonmail.com>
2024-04-14 19:15:13 +0300

    Fix replace keybind in mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -51,10 +51,10 @@ function M.config()
         mappings = {
             add = 'ys', -- Add surrounding in Normal and Visual modes
             delete = 'ds', -- Delete surrounding
+            replace = 'cs', -- Replace surrounding
             find = 'ysf', -- Find surrounding (to the right)
             find_left = 'ysF', -- Find surrounding (to the left)
             highlight = 'ysh', -- Highlight surrounding
-            replace = 'csr', -- Replace surrounding
             update_n_lines = 'ysn', -- Update `n_lines`
 
             suffix_last = 'l', -- Suffix to search with "prev" method
