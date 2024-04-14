bf8361c606b24b986efecf9bb8f12a234dff51f4
Finnbyte <eelpy@protonmail.com>
2024-04-14 17:31:42 +0300

    Fix mini.surround conflicting with leap.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -47,7 +47,20 @@ function M.config()
             ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
         },
     })
-    require("mini.surround").setup()
+    require("mini.surround").setup({
+        mappings = {
+            add = 'ys', -- Add surrounding in Normal and Visual modes
+            delete = 'ds', -- Delete surrounding
+            find = 'ysf', -- Find surrounding (to the right)
+            find_left = 'ysF', -- Find surrounding (to the left)
+            highlight = 'ysh', -- Highlight surrounding
+            replace = 'csr', -- Replace surrounding
+            update_n_lines = 'ysn', -- Update `n_lines`
+
+            suffix_last = 'l', -- Suffix to search with "prev" method
+            suffix_next = 'n', -- Suffix to search with "next" method
+        }
+    })
     require('mini.comment').setup({
         options = {
             -- Whether to ignore blank lines when adding comment
