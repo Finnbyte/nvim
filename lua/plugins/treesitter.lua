8516ad44b53ff31798a5cf21da6adfc9fdc7d37b
Finnbyte <eelpy@protonmail.com>
2024-04-14 15:53:09 +0300

    Add treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,58 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPost",
+    build = ":TSUpdate",
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "nu",
+            "bash",
+            "c",
+            "diff",
+            "html",
+            "javascript",
+            "jsdoc",
+            "json",
+            "jsonc",
+            "lua",
+            "luadoc",
+            "luap",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "yaml",
+            "rust",
+        },
+        sync_install = false,
+        additional_vim_regex_highlighting = false,
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-space>",
+                node_incremental = "<C-space>",
+                scope_incremental = false,
+                node_decremental = "<bs>",
+            },
+        },
+        textobjects = {
+            move = {
+                enable = true,
+                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+            },
+        },
+    },
+}
+
+return M
+
