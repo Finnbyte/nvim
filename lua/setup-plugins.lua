0f061b40e9e972dc1436d2075b1fe80c488cb74c
Finnbyte <eelpy@protonmail.com>
2024-04-14 15:22:44 +0300

    Add file to bootstrap and setup lazy

---
+++ lua/setup-plugins.lua
@@ -0,0 +1,38 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins", {
+  defaults = { lazy = true, version = "*" },
+  ui = { wrap = "true" },
+  checker = { enabled = true },
+  change_detection = {
+    enabled = true,
+    notify = true
+  },
+  debug = false,
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
