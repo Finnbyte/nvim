02c02c46b0721487a2041eb4a0cd3bb37d0e53ba
Finnbyte <eelpy@protonmail.com>
2024-04-14 17:31:31 +0300

    Setup mini.hipatterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -15,6 +15,18 @@ function M.config()
     require("mini.statusline").setup({
         use_icons = false
     })
+    local hipatterns = require("mini.hipatterns")
+    hipatterns.setup({
+        highlighters = {
+            fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+            hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+            todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+            note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+            -- Highlight hex color strings (`#rrggbb`) using that color
+            hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+    })
     require("mini.move").setup()
     require("mini.jump").setup()
     require("mini.pairs").setup({
