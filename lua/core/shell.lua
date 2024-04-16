7ef60e9aab07745506a618f1a1688371371dd899
Finnbyte <eelpy@protonmail.com>
2024-04-16 22:39:51 +0300

    Fix shell commands not working in Windows

--- lua/core/shell.lua
+++ lua/core/shell.lua
@@ -1,25 +1,24 @@
-local nu_shell = function ()
-  vim.cmd([[let &shell = 'nu']])
-  vim.cmd([[let &shellcmdflag = '-c']])
-  vim.cmd([[let &shellquote = '']])
-  vim.cmd([[let &shellxquote = '']])
+local nu_shell = function()
+    vim.cmd([[let &shell = 'nu']])
+    vim.cmd([[let &shellcmdflag = '-c']])
+    vim.cmd([[let &shellquote = '']])
+    vim.cmd([[let &shellxquote = '']])
 end
 
 local powershell = function()
-  vim.cmd([[set shell=powershell]])
-  vim.cmd([[set shellcmdflag=-command]])
-  vim.cmd([[set shellquote=\"]])
-  vim.cmd([[set shellxquote=]])
+    -- vim.cmd([[set shellcmdflag=-command]])
+    -- vim.cmd([[set shellquote=\"]])
+    -- vim.cmd([[set shellxquote=]])
 end
 
 local TRUE = 1
 
 if vim.fn.has("linux") == TRUE then
-  return
+    return
 end
 
 if vim.fn.executable('nu') == TRUE then
-  nu_shell()
+    nu_shell()
 else
-  powershell()
+    powershell()
 end
