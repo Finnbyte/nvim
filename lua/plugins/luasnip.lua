4aeaa4b3393c1141694f719c46e3023eacd4fdf9
Finnbyte <eelpy@protonmail.com>
2025-01-17 17:22:31 +0200

    Add flutter snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,13 +1,13 @@
 local M = {
-	"L3MON4D3/LuaSnip",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+    },
 }
 
 function M.config()
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip").filetype_extend("dart", { "flutter" })
 end
 
 return M
-
