77f3b15ed5d5137b52b6be3456c0c7ca3c4e7a09
Finnbyte <eelpy@protonmail.com>
2025-03-28 22:55:22 +0200

    Modify debugger

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,9 +1,7 @@
 -- Taken from kickstart.nvim
 
 local M = {
-    -- NOTE: Yes, you can install new plugins here!
     'mfussenegger/nvim-dap',
-    -- NOTE: And you can specify dependencies as well
     dependencies = {
         -- Creates a beautiful debugger UI
         'rcarriga/nvim-dap-ui',
@@ -84,35 +82,12 @@ local M = {
             -- see mason-nvim-dap README for more information
             handlers = {},
 
-            -- You'll need to check that you have the required things installed
-            -- online, please don't ask me how to install them :)
             ensure_installed = {
-                -- Update this to ensure that you have the debuggers for the langs you want
                 'delve',
             },
         }
 
-        -- Dap UI setup
-        -- For more information, see |:help nvim-dap-ui|
-        dapui.setup {
-            -- Set icons to characters that are more likely to work in every terminal.
-            --    Feel free to remove or use ones that you like more! :)
-            --    Don't feel like these are good choices.
-            icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-            controls = {
-                icons = {
-                    pause = '⏸',
-                    play = '▶',
-                    step_into = '⏎',
-                    step_over = '⏭',
-                    step_out = '⏮',
-                    step_back = 'b',
-                    run_last = '▶▶',
-                    terminate = '⏹',
-                    disconnect = '⏏',
-                },
-            },
-        }
+        dapui.setup()
 
         -- Change breakpoint icons
         -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
